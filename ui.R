e0be7717dbe75055801ef7a83555e2c6f28af3b9
Sharla Gelfand <sharla.gelfand@gmail.com>
2018-04-18 19:59:17 -0400

    Convert layout to multi-panel

--- ui.R
+++ ui.R
@@ -12,32 +12,40 @@ palette_names <- str_replace_all(palettes_df[["palette"]],
                                  "-",
                                  " ")
 
-fluidPage(
-  titlePanel("Eyeshadow palette generator"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      width = 4,
-      numericInput("n_eyeshadows", label = "Number of eyeshadows:",
-                   value = ""),
-      
-      radioButtons("type", label = "Choose from:",
-                   choices = list("All eyeshadows",
-                                  "Singles only", 
-                                  "Specific palette(s)"
-                   ),
-                   selected = NULL),
-      
-      conditionalPanel(
-        condition = "input.type == 'Specific palette(s)'",
-        checkboxGroupInput("palette", "",
-                           choices = palette_names)),
-      
-      actionButton("run", label = "Generate my palette!")
-    ),
-    
-    mainPanel(
-      imageOutput("my_palette")
+navbarPage(
+  title = "ryeshadow",
+  tabPanel(
+    "Randomizer",
+    fluidPage(
+      sidebarLayout(
+        sidebarPanel(
+          width = 4,
+          numericInput("n_eyeshadows", label = "Number of eyeshadows:",
+                       value = ""),
+          
+          radioButtons("type", label = "Choose from:",
+                       choices = list("All eyeshadows",
+                                      "Singles only", 
+                                      "Specific palette(s)"
+                       ),
+                       selected = NULL),
+          
+          conditionalPanel(
+            condition = "input.type == 'Specific palette(s)'",
+            checkboxGroupInput("palette", "",
+                               choices = palette_names)),
+          
+          actionButton("run", label = "Generate my palette!")
+        ),
+        
+        mainPanel(
+          imageOutput("my_palette")
+        )
+      )
     )
+  ),
+  tabPanel(
+    "Tracking",
+    fluidPage()
   )
 )
