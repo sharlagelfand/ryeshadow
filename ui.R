ee2d56886179752477464c3c033e17e55c8cb13e
Sharla Gelfand <sharla.gelfand@gmail.com>
2018-04-18 20:41:33 -0400

    Move pulling list of eyeshadows for UI to seperate file

--- ui.R
+++ ui.R
@@ -1,16 +1,4 @@
-# generating list of palettes to use in UI
-pans <- dir_ls("pans/", recursive = TRUE, glob = "*.png")
-
-eyeshadows_df <- data_frame(path = pans) %>%
-  separate(path,
-           into = c("folder", "type", "brand", "palette", "shade"),
-           sep = "/",
-           remove = FALSE) %>%
-  mutate_at(vars(brand:shade), funs(str_replace_all(., "-", " "))) %>%
-  mutate(shade = str_replace(shade, ".png", "")) 
-
-palette_names <- unique((eyeshadows_df %>%
-                           filter(type == "palettes"))[["palette"]])
+source("components/collection.r")
 
 navbarPage(
   title = "ryeshadow",
